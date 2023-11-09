03bcf5cb749c6008d9c5388d66b24fb78b920ff1
Team4 <emil.lehikoinen@outlook.com>
2023-11-09 14:12:21 +0200

    Initial Commit

---
+++ Event_App/Event_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_AppApp.swift
+//  Event_App
+//
+//  Created by iosdev on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Event_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
