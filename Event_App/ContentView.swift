f3829eab48000e6e2214f146f9b8c6c955f65c0b
Team4 <emil.lehikoinen@outlook.com>
2023-11-11 15:35:57 +0200

    Navigation start

--- Event_App/ContentView.swift
+++ Event_App/ContentView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+          VStack {
+                NavigationLink(destination: ContentView2()) {
+                    Text("Events")
+                }
+                .navigationBarTitle("Main page", displayMode: .inline)
+                Spacer()
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ Event_App/ContentView2.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView2.swift
+//  Event_App
+//
+//  Created by iosdev on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text("Events")
+            .navigationBarTitle("Events", displayMode: .inline)
+    }
+}
+
+struct ContentView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView2()
+    }
+}
