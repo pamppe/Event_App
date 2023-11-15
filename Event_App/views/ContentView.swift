2a5c20b1a53bd1a14584c6428406bbd180cfbcae
Lekematti <leo.koskimaki@gmail.com>
2023-11-15 14:25:40 +0200

    Moved contentView code to contenView2 & other stuff

--- Event_App/views/ContentView.swift
+++ Event_App/views/ContentView.swift
@@ -1,57 +1,16 @@
 import SwiftUI
 
-
 struct ContentView: View {
-    private var listOfEvents: [Event] = []
-    @State var searchText = ""
-    @State private var showMenu: Bool = false
-    
     var body: some View {
         NavigationView {
-            ZStack{
-                VStack{
-                    EventCardView()
-                        .navigationBarTitle("Main page", displayMode: .inline)
-                }
-                GeometryReader { _ in
-                    HStack {
-                        Spacer()
-                        SideMenuView()
-                            .offset(x: showMenu ? 0 : UIScreen.main.bounds.width)
-                    }
-                }
-                .background(Color.black.opacity(showMenu ? 0.5 : 0))
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                Button{
-                    self.showMenu.toggle()
-                }label: {
-                    if showMenu {
-                        Image(systemName: "xmark")
-                            .font(.title)
-                            .foregroundColor(.blue)
-                    }else {
-                        Image(systemName: "text.justify")
-                            .font(.title)
-                            .foregroundColor(.black)
-                    }
+            VStack{
+                NavigationLink(destination: ContentView2()){
+                    Text("Start")
                 }
             }
+            
         }
-        .searchable(text: $searchText)
     }
-    
-    
-    //    var events: [Event] {
-    //        let lcEvents = listOfEvents.map { $0.nameInLanguage().lowercased() }
-    //
-    //        return searchText.isEmpty ? listOfEvents : listOfEvents.filter {
-    //            $0.nameInLanguage().lowercased().contains(searchText.lowercased())
-    //        }
-    //    }
-    
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
