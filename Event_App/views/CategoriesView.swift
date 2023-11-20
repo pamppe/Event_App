aba5a8d2e8825d611e24b9c1c638e2913e375a7e
Lekematti <leo.koskimaki@gmail.com>
2023-11-20 19:18:36 +0200

    Etusivu, kategoriat ja tietoa meistä nappit/sivut lisätty sivumenuun. Korjattu myös navigation back button.

---
+++ Event_App/views/CategoriesView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesView.swift
+//  Event_App
+//
+//  Created by iosdev on 20.11.2023.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoriesView()
+    }
+}
