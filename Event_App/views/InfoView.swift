12b327f1e8ca04d60d55c60e13f8129af25b7e03
Lekematti <leo.koskimaki@gmail.com>
2023-11-20 19:24:33 +0200

    Info sivu ja navigaatio lisätty

---
+++ Event_App/views/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  Event_App
+//
+//  Created by iosdev on 20.11.2023.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text("Tietoa meistä")
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
