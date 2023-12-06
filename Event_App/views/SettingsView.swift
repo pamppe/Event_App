3436ac1a2d07c4aee1f52de202585837be174443
Lekematti <leo.koskimaki@gmail.com>
2023-12-06 23:52:36 +0200

    Petterin filut laitettu tänne branchiin, pieniä muokkauksia ja sovituksia tehty. (ei valmis)

--- Event_App/views/SettingsView.swift
+++ Event_App/views/SettingsView.swift
@@ -1,3 +1,6 @@
+/// SETTINGSVIEW
+
+
 //
 //  SettingsView.swift
 //  Event_App
@@ -8,8 +11,47 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var activeTab: Int = 0
+    /// Sample toggle states
+    @State private var toggles: [Bool] = Array(repeating: false, count: 10)
+    /// Interface style
+    @State private var toggleDarkMode: Bool = false
+    @State private var activateDarkMode: Bool = false
+    @State private var buttonRect: CGRect = .zero
     var body: some View {
-        Text("settings page")
+        /// Sample view
+        TabView(selection: $activeTab) {
+            NavigationStack {
+                List {
+                    Section("Language") {
+                        Toggle("English", isOn: $toggles[0])
+                        Toggle("Finnish", isOn: $toggles[1])
+                    }
+                    Section {
+                    } footer: {
+                        Text("Sample footer")
+                    }
+                }
+                .navigationTitle("Settings")
+            }
+        }
+        .overlay(alignment: .topTrailing) {
+            Button(action: {
+                toggleDarkMode.toggle()
+            }, label: {
+            Image(systemName: toggleDarkMode ?  "sun.max.fill" : "moon.fill")
+                .font(.title2)
+                .foregroundStyle(Color.primary)
+                
+                .frame(width: 40, height: 40)
+            })
+                .padding(10)
+                .background {
+                    GeometryReader(content: { geometry in
+                        
+                    })
+                }
+        }
     }
 }
 
